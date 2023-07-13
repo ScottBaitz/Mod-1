204e4ed79405ba4f9944b37e942b102aeacbb714
Scott Baitz <scott@baitz.xyz>
2023-07-13 15:06:28 -0600

    Solo practice, book & library classes

--- objects_classes_and_instances/bookrunner.rb
+++ objects_classes_and_instances/bookrunner.rb
@@ -4,5 +4,9 @@ require 'pry'
 book_1 = Book.new("Where the weed at", "Pot McSmokin", "Autobiography")
 book_2 = Book.new("Time and Timeability", "That one author", "Classic Fiction")
 book_3 = Book.new("Hella", "California Dude", "Sci-Fi")
+
+
+library = Library.new("My Library", 10, [book_1, book_2, book_3])
+library.print_books
+
 binding.pry
-a=1
--- objects_classes_and_instances/books.rb
+++ objects_classes_and_instances/books.rb
@@ -9,9 +9,24 @@ end
 
 class Library
     attr_reader :collection, :name, :size
-    def initialize(collection, name, size)
-        @collection = collection
-        @name = name
-        @size = size
+  
+    def initialize(name, size, collection = [])
+      @collection = collection
+      @name = name
+      @size = size
     end
-end
+  
+    def print_books
+      puts "Books in the #{name} library:"
+      puts "------------------------------------"
+      printf "%-25s %s\n", "Title", "Author"
+      puts "------------------------------------"
+  
+      collection.each do |book|
+        printf "%-25s %s\n", book.title, book.author
+      end
+  
+      puts "------------------------------------"
+    end
+  end
+  
